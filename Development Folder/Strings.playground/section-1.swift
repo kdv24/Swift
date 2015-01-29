76c64cdd4c3a7ba9e14049366b5c09ec8c0cf45b
Kelly de Vries <kdv@me.com>
2015-01-29 08:54:07 -0800

    Scrapping multiple files

--- Development Folder/Strings.playground/section-1.swift
+++
@@ -1,8 +0,0 @@
-// Playground - noun: a place where people can play
-
-import UIKit
-
-println("List:\n1\t🍎\n2\tBananas\n3\tOranges")
-
-println("Kelly's favorites:\n\tfamily\n\tfriends\n\tflowers")
-
--- Development Folder/Temperature Converter challenge.playground/section-1.swift
+++
@@ -1,9 +0,0 @@
-// Playground - noun: a place where people can play
-
-import UIKit
-
-var fahrenheit = 32
-
-var celsius = ((fahrenheit - 32) * 5) / 9
-
-
